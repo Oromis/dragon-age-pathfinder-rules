269989b1e2dd247dfd7d6a4842d803cb087efd3c
David Bauske <david.bauske@voltstorage.com>
2026-02-03 20:45:37 +0100

    Fixed base URL

--- quartz.config.ts
+++ quartz.config.ts
@@ -16,7 +16,7 @@ const config: QuartzConfig = {
       provider: "plausible",
     },
     locale: "en-US",
-    baseUrl: "quartz.jzhao.xyz",
+    baseUrl: "https://oromis.github.io/dragon-age-pathfinder-rules",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "modified",
     theme: {
